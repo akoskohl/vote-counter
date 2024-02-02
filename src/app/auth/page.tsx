921439a75c452be17943ece6d3280c60d97b81df
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-02 10:16:22 +0100

    firebase auth base config

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,25 @@
+import firebase from "firebase/compat/app";
+import firebaseui from "firebaseui";
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
+
+export default function Page() {
+  return (
+    <div className="auth">
+      <div id="firebaseui-auth-container"></div>
+    </div>
+  )
+}
