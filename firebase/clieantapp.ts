61e24585016a24585f8ecd4e64a6d558dcc66991
akos.kohl@t-systems.com <154735628+akoskohl@users.noreply.github.com>
2024-02-05 15:15:42 +0100

    config moved to source dir

--- firebase/clieantapp.ts
+++ firebase/clieantapp.ts
@@ -1,17 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import firebase from 'firebase/compat/app';
-import "firebase/auth";
-import "firebase/firestore";
+// import { initializeApp } from 'firebase/app';
+// import firebase from 'firebase/compat/app';
+// import "firebase/auth";
+// import "firebase/firestore";
 
-const clientCredentials = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGIN_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+// const clientCredentials = {
+//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGIN_SENDER_ID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
 
-const app = initializeApp(clientCredentials);
+// const app = initializeApp(clientCredentials);
 
-export default firebase;
+// export default firebase;
